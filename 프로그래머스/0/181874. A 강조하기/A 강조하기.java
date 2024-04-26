154f0a5a8f24b6ad9ff60c6b943dfdeec9052cf4
JSCU0403 <134984859+JSCU0403@users.noreply.github.com>
2024-04-26 21:52:57 +0900

    [level 0] Title: A 강조하기, Time: 0.08 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        myString = myString.toLowerCase();
+        String answer = myString.replaceAll("a","A");
+        return answer;
+    }
+}
