a0ecc220c8cf4382318b963389b9d3733943804d
JSCU0403 <134984859+JSCU0403@users.noreply.github.com>
2024-06-04 20:26:43 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 10.42 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(list.size() == k) break;
+            
+            if(!list.contains(arr[i])) list.add(arr[i]);
+        }
+        
+        while(list.size() < k){
+            list.add(-1);
+        }
+        
+        return list.stream().mapToInt(j->j).toArray();
+    }
+}
